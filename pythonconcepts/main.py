0eccb772cd7e3aaad9bec75a1fcfe1fcc700004e
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-07 22:37:43 +0530

    Implemented basics
    This repository contains Python programs and code snippets that showcase various concepts,
    ranging from basics to advanced topics. It also includes practice sets and examples
    to reinforce key ideas and improve mastery.
    
    I studied Python, Pandas, MySQL, and many related concepts during my high school since
    even before age 13, so I already have a solid foundation.
    
    This repository serves as a quick recap & continuation of my learning journey,
    with a focus on mastering Python and exploring its vast potential further.

---
+++ pythonconcepts/main.py
@@ -0,0 +1,47 @@
+"""
+This repository contains Python programs and code snippets that showcase various concepts,
+ranging from basics to advanced topics. It also includes practice sets and examples
+to reinforce key ideas and improve mastery.
+
+I studied Python, Pandas, MySQL, and many related concepts during my high school since
+even before age 13, so I already have a solid foundation.
+
+This repository serves as a quick recap & continuation of my learning journey,
+with a focus on mastering Python and exploring its vast potential further.
+"""
+
+print("Hello World!~~~~~~~")
+
+
+# Basics~~~
+a = 0
+b = True
+c = "Natsuki"
+d = None
+e = complex(8, 2)
+print(b)
+print(c)
+print(e)
+a1 = 9
+print( + a1)
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+
+list1 = [444, 2.22, [-4, 5]]
+print(list1)
+
+tuple = (("AOT", "JJK"), ("Attack on Titan", "Jujutsu Kaisen"))
+print(tuple)
+
+dict1 = {"name":"Natsuki", "age":17, "canVote":False}
+print(dict1)
+
+# Operators :-
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
