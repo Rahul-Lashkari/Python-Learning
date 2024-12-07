4f01e1f1f4b5065a321e3c277a590961b80a6f90
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-07 23:22:23 +0530

    Strings
    Strings in Python

---
+++ pythonconcepts/Strings.py
@@ -0,0 +1,19 @@
+name = "Rahul"
+friend = "Shibu"
+anotherFriend = 'Priyanka'
+randomtxt = '''He said, 
+Hi Rahul
+hey I am good
+I want to eat an apple'''
+
+print("Hello, " + name)
+print(randomtxt)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # Throws an error
+print("Lets use a for loop\n")
+for character in randomtxt:
+    print(character)
