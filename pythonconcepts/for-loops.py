edd464e7acb7789a75ae9c43153259add3904f27
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-08 23:29:56 +0530

    For Loops

---
+++ pythonconcepts/for-loops.py
@@ -0,0 +1,20 @@
+# name = 'Aaditya'
+# for i in name:
+#     print(i)
+#     if(i == "t"):
+#         print("This is something special!")
+
+# colors = "Red", "Green", "Blue", "Yellow"
+# for i in colors:
+#     print(i)
+#     for j in i:
+#         print(j)
+
+# for k in range(0, 5):
+#     print(k + 1)
+
+# for k in range(1, 222):
+#     print(k + 1)
+
+for k in range(0, 11, 2):
+    print(k)
