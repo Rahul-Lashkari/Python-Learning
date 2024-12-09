4a80ef74c0482a8a29c5e7da353ea51267f16d33
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-09 22:35:12 +0530

    Functions in Python

---
+++ pythonconcepts/Functions.py
@@ -0,0 +1,38 @@
+def calculateGmean(a, b):
+    mean = (a * b) / (a + b)
+    print(mean)
+
+def isGreater(a, b):
+    if(a>b):
+        print("First Number is Greater")
+    else:
+        print("Second number is greater or equal")
+
+def isLesser(a, b):
+    pass
+
+# a = 9
+# b = 8
+# isGreater(a, b)
+# calculateGmean(a, b)
+# # gmean1 = (a*b)/(a+b)
+# # print(gmean1)
+# c = 8
+# d = 74
+# isGreater(c, d)
+# calculateGmean(c, d)
+# # gmean2 = (c*d)/(c+d)
+# # print(gmean2)
+
+a = 9
+b = 8
+isGreater(a, b)
+calculateGmean(a, b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+c = 8
+d = 80
+isGreater(c, d)
+calculateGmean(c, d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
