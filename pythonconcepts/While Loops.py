9039694db80c5a4f848fe36f4c56bb9c797d4ba5
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-09 09:46:39 +0530

    While Loops

---
+++ pythonconcepts/While Loops.py
@@ -0,0 +1,16 @@
+i = int(input("Enter the number: "))
+print(i)
+while(i<=40):
+    i = int(input("Enter the number: "))
+    print(i)
+
+print("Done with the loop")
+
+
+print("\nThis is another loop:-")
+count = 5
+while(count > 0 ):
+    print(count)
+    count = count - 1
+else:
+    print("I'm inside else")
