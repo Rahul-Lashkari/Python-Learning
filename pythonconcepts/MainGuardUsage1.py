c73bd794c29331c05e07d50de41f92c54741991d
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-14 09:53:53 +0530

    Explored if __name__ == "__main__" in Python
    Demonstrated the use of if __name__ == "__main__" to execute code conditionally when a Python script is run directly, ensuring modularity and reusability.

---
+++ pythonconcepts/MainGuardUsage1.py
@@ -0,0 +1,5 @@
+# Context: Explored if __name__ == "__main__" in Python
+
+import MainGuardUsage2
+
+MainGuardUsage2.welcome()
---
+++ pythonconcepts/MainGuardUsage2.py
@@ -0,0 +1,9 @@
+# Context: Explored if __name__ == "__main__" in Python
+
+def welcome():
+  print("Hey you are welcome from Python")
+
+# print(__name__)
+
+if __name__ == "__main__":
+  welcome()
