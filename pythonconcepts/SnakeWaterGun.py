2d8084499b825c1ec1e0b062f1e4c96ccc66770f
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-23 21:22:08 +0530

    Ex5: Snake Water Gun Game
    Develop a Python program for the classic Snake Water Gun game. Implement user-vs-computer gameplay logic with random choices and result evaluation.

---
+++ pythonconcepts/SnakeWaterGun.py
@@ -0,0 +1,9 @@
+# Exercise 5: Snake Water Gun Game ~~~
+
+# Structure :-
+
+#                 S W G
+# computer =      0 1 2
+# player =  S  0  D W L
+#           W  1  L D W
+#           G  2  W L D
